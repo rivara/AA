9f68c1ad7f6ac3c829d53c58d875681ba70377d4
rivara <ricardo.v.ramirez@gmail.com>
2017-10-20 18:42:57 +0200

    20/10/2017 bis

--- src/Ejecicio/Ejercicio2.java
+++ src/Ejecicio/Ejercicio2.java
@@ -2,50 +2,57 @@ package Ejecicio;
 
 public class Ejercicio2 {
 
-	public static int sedes1 (int[] c0, int[] c1, int f) {
-	//int[] result= new int[f+c0.length+c1.length];
-	int result=0;
-	boolean esta=false;
-		for(int d =0; d < c0.length; d ++ ) {
-			
-			//if a >b coge a
-			if(c0[d]<=c1[d]) {
-				result=result+c0[d];
-				esta=false;
-			}else {
-				if(esta) {
-					result=result+c0[d];
-					esta=true;
-				}else {
-					
+	public static int sedes1(int[] c0, int[] c1, int f) {
+		// int[] result= new int[f+c0.length+c1.length];
+		int result = 0;
+		boolean esta = false;
+		for (int d = 0; d < c0.length; d++) {
+
+			// if a >b coge a
+			if (c0[d] <= c1[d]) {
+				result = result + c0[d];
+
+				esta = false;
+			} else {
+				if (!esta) {
+					result = result + f;
+					esta = true;
+				} else {
+
 				}
 			}
 		}
-			
-			
-			/*
-			//suma 
-			// if a<b coge b
-			if(c0[d]>c1[d]) {
-				result=result+c1[d];
-			}else {
-				result=result+f;
+
+		esta = false;
+		for (int d = 0; d < c0.length; d++) {
+
+			// if a<b coge a
+			if (c0[d] >= c1[d]) {
+
+				result = result + c1[d];
+
+				esta = false;
+			} else {
+				if (!esta) {
+					result = result + f;
+					esta = true;
+				} else {
+
+				}
 			}
-			//suma
-			result=result+f;
-		}*/
-		
-		return result;
+		}
+
+		return result - f;
 	}
 	/*
-	public static int sedes2 (int[] c0, int[] c1, int f) {}*/
-	
+	 * public static int sedes2 (int[] c0, int[] c1, int f) {}
+	 */
+
 	public static void main(String[] args) {
-		  int c0[]={1,3,20,30};
-		  int c1[]={50,20,2,4};
-		  int f=10;
-		  System.out.print(sedes1(c1,c0,f));
-		  
+		int c0[] = { 1, 3, 20, 30 };
+		int c1[] = { 50, 20, 2, 4 };
+		int f = 10;
+		System.out.print(sedes1(c0, c1, f));
+
 	}
 }
-
