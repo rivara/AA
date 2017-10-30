c2d4240f97eb11fa8e64cd2f6a4fcd3f632e4b13
rivara <ricardo.v.ramirez@gmail.com>
2017-10-30 20:44:06 +0100

    30/10/2017 (2)

--- src/Ejecicio/Ejercicio3.java
+++ src/Ejecicio/Ejercicio3.java
@@ -2,4 +2,76 @@ package Ejecicio;
 
 public class Ejercicio3 {
 
+	public static int sedes (int[] c0, int[] c1, int f){
+		System.out.println(buscarOptima(f, f, c1, f, c1, f));
+		return f;
+		
+	}
+	
+	
+	
+	// vuelta atras
+	
+		private static int buscarOptima(int n, int i,int[ ] solActual, int valorActual,int[ ] solOptima, int valorOptimo) {
+		int valor;
+		for (int k=0; k<solActual.length-1; k++) {
+		//<<generar candidato k-ésimo>>;
+		if (1==1){//<<candidato válido>>) {
+			//<<incluirlo en solActual y actualizar valorActual>>;
+				if (i==n){
+						if (valorActual>valorOptimo) {
+								//<<solOptima = solActual>>
+								valorOptimo = valorActual;
+						}else{
+									
+		valorOptimo = buscarOptima(n, i+1, solActual, valorActual,solOptima, valorOptimo);
+		//<<borrarlo de solActual y restaurar valorActual>>;
+		}
+		}
+		}
+		}
+		return valorOptimo;
+		}
+	
+	
+// ramificación y poda
+		
+		/*private static int buscarOptima
+(int n_1, int i,
+Valor[ ] solActual, int valorActual,
+Valor[ ] solOptima, int valorOptimo) {
+int valor;
+for (int k=0; k<m; k++) {
+<<generar candidato k-ésimo>>;
+if (<<candidato válido>>) {
+<<calcular cota superior>>
+if cota > valorOptimo {
+<<incluirlo en solActual y actualizar valorActual>>;
+if (i==n_1)
+if (valorActual>valorOptimo) {
+<<solOptima = solActual>>
+valorOptimo = valorActual;
+{
+else
+valorOptimo = buscarOptima
+(n, i+1, solActual, valorActual,
+solOptima, valorOptimo);
+<<borrarlo de solActual y restaurar valorActual>>;
+}
+}
+}
+return valorOptimo;
+}*/
+	public static void main(String[] args) {
+		int c0[] = { 3, 8, 5, 2, 4 };
+		int c1[] = { 7, 1, 6, 9, 6 };
+		int f = 5;
+		
+		sedes(c0,c1,f);
+		
+		
+
+	}
+
+	
 }
