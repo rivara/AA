760e092fcbe0157875d0d3d6630230dd6334ded4
rivara <ricardo.v.ramirez@gmail.com>
2017-11-09 21:41:30 +0200

    9/11/2018

--- src/Ejecicio/Ejecicio3_1_b.java
+++ src/Ejecicio/Ejecicio3_1_b.java
@@ -2,22 +2,9 @@ package Ejecicio;
 
 class Ejecicio3_1_b {
 
-
+static int nodos;
 	public static int sedes_RyP(int[] c0, int[] c1, int f) {
-		/*int[] solParcial = new int[c0.length];
-		int[] solOptima = new int[c0.length];
-		int coste[][]={c0, c1};  
-	
-		// Calculamos la cota
-		int cota = calcCota(c0, c1, f, solParcial, 0);
-		System.out.println("Cota inicial: " + cota
-				+ ". Coste mínimo inicial: 0");
-		int cOpt = buscarSedes_RyP(c0, c1, f, 0, solParcial, 0, solOptima,Integer.MAX_VALUE, cota);
-	
-		//(c0, c1, f, etapa + 1, solActual,valorActual, solOptima, valorOptimo);*/
-	
-	     ///////////////////////////////
-		//////////////////////////////
+
 		
 		 int[] solParcial = new int[c0.length];
 	      int[] solOptima = new int[c0.length];
@@ -35,129 +22,7 @@ class Ejecicio3_1_b {
 
 
 	/*ramifica y poda*/
-	private static int buscarSedes_RyP(int[] c0, int[] c1, int f, int etapa,
-			int[] solParc, int cParcial, int[] solOpt, int cOpt, int cota) {
-		// Se recorren las dos sedes existentes
-		for (int j = 0; j < 2; j++) {
-			solParc[etapa] = j;
-			// Calculamos la cota en cada nivel
-			int nCota = calcCota(c0, c1, f, solParc, etapa);
-			/*
-			 * Evaluamos etapa la cota es menor que el mejor candidato hasta el
-			 * momento. En caso de ser mayor, no continuará la evaluación,
-			 * podando la rama
-			 */
-			if (nCota < cOpt) {
-				// Sumamos el valor correspondiente al nuevo nivel
-				cParcial += coste(c0, c1, f, solParc, etapa);
-				/*
-				 * Comprobamos setapa hemos llegado al final de la rama, para fijar
-				 * el mejor candidato
-				 */
-				if (etapa == c0.length - 1) {
-					if (cParcial < cOpt) {
-						cOpt = cParcial;
-						for (int k = 0; k < solParc.length; k++)
-							solOpt[k] = solParc[k];
-					/*	System.out.print("Nuevo coste mínimo: " + cOpt
-								+ ". Estado: ");
-						imprimir(solOpt);*/
-					}
-				}
-				/*
-				 * Setapa no hemos llegado al final de la rama, seguimos bajando
-				 * niveles de forma recursiva
-				 */
-				else
-					cOpt = buscarSedes_RyP(c0, c1, f, etapa + 1, solParc, cParcial,
-							solOpt, cOpt, cota);
-				// Restamos el valor correspondiente, para continuar con otra
-				// rama
-				cParcial -= coste(c0, c1, f, solParc, etapa);
-				System.out.println("opcion:"+j+" etapa:"+etapa+" coste:"+cota);
-			} else {
-				
-				// System.out.print("Poda con cota: " + nCota + ". Estado: ");
-				// imprimir2(solParc, etapa);
-			}
-		}
-		return cOpt;
-	}
-
-	/*
-	 * Algoritmo que calcula la cota. Fijará el valor real consumido hasta cada
-	 * nodo y, a partir de ahí, comparará el mejor valor de los hijos hasta
-	 * llegar a cada nodo hoja
-	 */
-	public static int calcCota(int[] c0, int[] c1, int f, int[] solParcial,
-			int etapa) {
-		int cota = 0;
-		for (int j = 0; j <= etapa; j++) {
-			cota += coste(c0, c1, f, solParcial, j);
-		}
-		for (int j = etapa + 1; j < c0.length; j++) {
-			cota += minimo(c0, c1, j);
-		}
-		return cota;
-	}
-
-	private static int minimo(int[] c0, int[] c1, int j) {
-		int min = 0;
-		if (c0[j] <= c1[j])min = c0[j];
-		else
-			min = c1[j];
-		return min;
-	}
-
-	private static int coste(int[] c0, int[] c1, int f, int[] solParcial, int etapa) {
-		/* Setapa estamos en la sede 0, asignamos a coste el valor correspondiente */
-		int coste = 0;
-		if (solParcial[etapa] == 0) {
-			coste = c0[etapa];
-			/*
-			 * Controlar setapa es necesario sumar cambio de sede, comparando setapa la
-			 * sede actual es igual a la anterior o setapa estamos en el primer
-			 * nivel
-			 */
-			if ((etapa != 0) && (solParcial[etapa] != solParcial[etapa - 1])) {
-				coste += f;
-			}
-			/*
-			 * Setapa estamos en la sede 1, asignamos a coste el valor
-			 * correspondiente
-			 */
-		} else {
-			coste = c1[etapa];
-			/*
-			 * Controlar setapa es necesario sumar cambio de sede, comparando setapa la
-			 * sede actual es igual a la anterior o setapa estamos en el primer
-			 * nivel
-			 */
-			if ((etapa != 0) && (solParcial[etapa] != solParcial[etapa - 1])) {
-				coste += f;
-			}
-		}
-		return coste;
-	}
-
-	private static void imprimir(int[] v) {
-		for (int etapa = 0; etapa < v.length; etapa++)
-			System.out.print(v[etapa] + " ");
-		System.out.println();
-	}
-
-	private static void imprimir2(int[] v, int k) {
-		for (int etapa = 0; etapa <= k; etapa++)
-			System.out.print(v[etapa] + " ");
-		for (int etapa = k + 1; etapa < v.length; etapa++)
-			System.out.print("- ");
-		System.out.println();
-	}
-	
-	
-	
-	
-	 private static int buscar2_Nueva (int[][] coste, int i, int f, int[] solParc, int cParc, int[] solOpt, int cOpt, int cota) 
+	private static int buscar2_Nueva (int[][] coste, int i, int f, int[] solParc, int cParc, int[] solOpt, int cOpt, int cota) 
 	   {
 		   		// versión de ramificación y poda, tomando como cota inferior el coste mínimo de costes de sedes sin traslado
 		   			
@@ -169,7 +34,7 @@ class Ejecicio3_1_b {
 		   				
 		   				if (nCota < cOpt) 
 		   				{
-		   					//nodos++;
+		   					nodos++;
 		   					
 		   					int gasto=coste[j][i];
 		   					
@@ -214,6 +79,12 @@ class Ejecicio3_1_b {
 		         if (coste[i][j]<min)
 		            min = coste[i][j];
 		      return min;
+	   }
+
+	   private static void imprimir (int[] v) {
+	      for (int i=0; i<v.length; i++)
+	         System.out.print (v[i]+" ");
+	      System.out.println();
 	   }
 
 	public static void main(String[] args) {
--- src/Ejecicio/Ejercicio3_1_a.java
+++ src/Ejecicio/Ejercicio3_1_a.java
@@ -3,90 +3,60 @@ package Ejecicio;
 public class Ejercicio3_1_a {
 
 	static int nodes;
+
 	public static int sedes(int[] c0, int[] c1, int f) {
 		int[] solActual = new int[c0.length];
-		int[] solOptima = new int[c0.length];
-		int coste[][]={c0, c1};  
-	    int cOpt = buscar (coste, 0, f, solActual, 0, solOptima, Integer.MAX_VALUE);
-		
-	    return cOpt;
+		int[] solOptimaima = new int[c0.length];
+		int cost[][] = { c0, c1 };
+		int cOptimaima = buscar(cost, 0, f, solActual, 0, solOptimaima, Integer.MAX_VALUE);
+		imprimir(solOptimaima);
+		return cOptimaima;
 	}
-	
 
+	private static int buscar(int[][] coste, int etapa, int f, int[] solActual, int cParcial, int[] solOptima,int cOptima) {
+		// versión basada en el esquema de la técnica de vuelta atrás para solución optima
+		for (int j = 0; j < coste.length; j++) {
+			nodes++;
+			solActual[etapa] = j;
+			int gasto = coste[j][etapa];
 
-	private static int buscar (int[][] coste,int etapa, int f,int[] solParc, int cParc, int[] solOpt, int cOpt) {
-		
-		
-		
-		// versión basada en el esquema de la técnica de vuelta atrás para solución óptima   
-			   	   for (int j=0; j<coste.length; j++)
-			   	   {
-			   		nodes++;
-			   		   solParc[etapa] = j;
-		   			   int gasto= coste[j][etapa];
-		   			
-		   			   if(etapa > 0 && solParc[etapa-1]!=j) //Setapa hay traslado
-			   			   gasto += f;
-		   			   
-		   			   cParc += gasto;
-		   			   
-			   		   if (etapa==solOpt.length-1) {
-			   			
-			   			   if (cParc<cOpt) {
-			   				   cOpt = cParc;
-			   			   }
-			   		   }
-			   		   else 
-			   			
-			   			   cOpt = buscar (coste, etapa+1, f, solParc, cParc, solOpt, cOpt);
-			   		 System.out.println("opcion:"+j+" etapa:"+etapa+" coste:"+cParc);
-			   		   //restaurar valorActual
-			   		   cParc -= gasto;  
-	  		   	   }
-			   	   return cOpt;
-	   }
+			if (etapa > 0 && solActual[etapa - 1] != j) // Setapa hay traslado
+				gasto += f;
 
-	
-	/// Método extra añade costes cuando es necesario
-	
-	private static int coste(int[] c0, int[] c1, int f, int[] solActual, int etapa) {
-		int coste = 0;
-		if (solActual[etapa] == 0) {
-			coste = c0[etapa];
-			/*
-			 * suma cambio de sede
-			 */
-			if ((etapa != 0) && (solActual[etapa] != solActual[etapa - 1])) {
-				coste += f;
-			}
-			/*
-			 * asignanar a coste el valor correspondiente
-			 */
-		} else {
-			coste = c1[etapa];
-			/*
-			 * Ver setapa es necesario sumar cambio de sede
-			 */
-			if ((etapa != 0) && (solActual[etapa] != solActual[etapa - 1])) {
-				coste += f;
-			}
+			cParcial += gasto;
+
+			if (etapa == solOptima.length - 1) {
+
+				if (cParcial < cOptima) {
+					cOptima = cParcial;
+					// aqui actualizo el array de la solucion optima
+					for (int k = 0; k < solActual.length; k++)
+						solOptima[k] = solActual[k];
+				}
+
+			} else
+
+			cOptima = buscar(coste, etapa + 1, f, solActual, cParcial, solOptima, cOptima);
+			//System.out.println("opcion:" + j + " etapa:" + etapa + " coste:" + cParcial);
+			// restaurar valorActual
+			cParcial -= gasto;
 		}
-		return coste;
-
+		return cOptima;
 	}
-	
-	
-	
+
+	private static void imprimir(int[] v) {
+		for (int i = 0; i < v.length; i++)
+			System.out.print(v[i] + " ");
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
 
 		int c0[] = { 1, 3, 20, 30 };
 		int c1[] = { 50, 20, 2, 4 };
-		//int c0[] = { 9, 3, 7, 30 };
-		//int c1[] = { 2, 20, 2, 3 };
 		int f = 10;
 		System.out.println(sedes(c0, c1, f));
-
+		System.out.println("numero de nodos creados: " + nodes);
 	}
 
 }
