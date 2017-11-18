f15de1de9d301d4e10e06fe6dedda1f27fc417cf
rivara <ricardo.v.ramirez@gmail.com>
2017-11-18 12:51:08 +0100

    18/11/2017  2

--- src/Ejecicio/Ejercicio4.java
+++ src/Ejecicio/Ejercicio4.java
@@ -7,29 +7,31 @@ public class Ejercicio4 {
 		if (y == 0)
 			return x;
 		else {
+			System.out.println("z"+x+" "+y);
 			return f(x, y - 1) + f(x + 1, y - 1);
 		}
 	}
-///////////////////////////////////////////////////////////////////
-// memorización //////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////
-	
-	
-	
+///////////////////////////////////////
+// memorización////////////////////////
+///////////////////////////////////////
+
 	// principal
 	private static void fibM(int x, int y, int z, int[][] fibs) {
 		if (fibs[z][y] == -1)
-			if (y == 0)
+			if (y == 0){
 				fibs[z][y] = x;
-			else {
+				//System.out.println("z"+x+" "+y);
+			}else {
 				fibM(x, y - 1, z, fibs);
 				fibM(x + 1, y - 1, z + 1, fibs);
+				//System.out.println("z"+x+" "+y);
 				fibs[z][y] = fibs[z][y - 1] + fibs[z + 1][y - 1];
+				
 			}
 	}
 
 	// auxiliar
-	public static int fM(int x, int y) {
+	public static int fib(int x, int y) {
 		int[][] fibs = new int[x + 2][y + 2];
 		for (int i = 0; i <= x; i++)
 			for (int z = 0; z <= x; z++)
@@ -37,14 +39,16 @@ public class Ejercicio4 {
 		fibM(x, y, 0, fibs);
 		return fibs[0][y];
 	}
-
-///////////////////////////////////////////////////////////////////
-//tabulación //////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////
-	public static int fT(int x, int y) {
-		int[][] fs = new int[y + 1][y + 1];
+	
+	
+///////////////////////////////////////
+//tabulación////////////////////////
+///////////////////////////////////////
+	public static int fTab(int x, int y) {
+		int[][] fs = new int[x + 1][y + 1];
 		// Calculamos valor inicial en (0,0)
 		fs[0][0] = x;
+		// Calculamos el resto de valores base de la fila 0
 		for (int i = 1; i <= y; i++)
 			fs[i][0] = fs[i - 1][0] + 1;
 		for (int j = 1; j <= y; j++) {
@@ -56,10 +60,13 @@ public class Ejercicio4 {
 		
 	
 	
+	
 	public static void main(String[] args) {
 		System.out.println(f(3,3));
-		System.out.println(fM(3,3));
-		System.out.println(fT(3,3));
+		System.out.println(fib(3,3));
+		System.out.println(fTab(3,3));
+		//System.out.println(fi(4));
+		//System.out.println(f2(4));
 	}
 
 }
