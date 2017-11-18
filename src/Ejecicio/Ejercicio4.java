c1d91e22d7940b0faaa87e332e989005a56270eb
rivara <ricardo.v.ramirez@gmail.com>
2017-11-18 11:38:49 +0100

    18/11/2017

--- src/Ejecicio/Ejercicio4.java
+++ src/Ejecicio/Ejercicio4.java
@@ -6,95 +6,60 @@ public class Ejercicio4 {
 	public static int f(int x, int y) {
 		if (y == 0)
 			return x;
-		else{
+		else {
 			return f(x, y - 1) + f(x + 1, y - 1);
 		}
 	}
-	//memorización 
-
+///////////////////////////////////////////////////////////////////
+// memorización //////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 	
-//principal
-	private static void fibM(int x,int y, int[] fibs) {
-		if (fibs[x] == -1)
-			if (y == 0) 
-				fibs[x] = x;
+	
+	
+	// principal
+	private static void fibM(int x, int y, int z, int[][] fibs) {
+		if (fibs[z][y] == -1)
+			if (y == 0)
+				fibs[z][y] = x;
 			else {
-				fibM(x ,y-1, fibs);
-				fibM(x +1,y-1, fibs);
-				fibs[x] = (fibs[x]+fibs[x+1])+(fibs[x]+fibs[x+1]);
+				fibM(x, y - 1, z, fibs);
+				fibM(x + 1, y - 1, z + 1, fibs);
+				fibs[z][y] = fibs[z][y - 1] + fibs[z + 1][y - 1];
 			}
 	}
-	
-	//auxiliar
-	public static int fib (int x, int y) {
-		int[] fibs = new int[x+2];
-			for (int i=0; i<=x; i++)
-					fibs[i] = -1;
-		fibM(x,y,fibs);
-		return fibs[x];
-		}
 
-	// tabulación
-///////////////////////////////////////////////////////////////7777
-////////////////////////////////////////////////////////////////
-	/*
-	public class Practica_4_ER {
-		public static int f (int x, int y) {
-		int[][] fs = new int[y + 1][y + 1];
-		for (int i=0; i<=y; i++)
-		for (int j=0; j<=y; j++)
-		fs[i][j] = -1;
-		fMem (x, y, fs, 0);
-		return fs[0][y];
-		}
-		 Aplicamos la tabla int[][] fs = new int [y] [x - xinicial]
-		Para conseguir esto, X1 será el contador que va incrementándose para
-		recorrer las posiciones de la matriz en el orden adecuado, actuando
-		como x - xinicial 
-		public static void fMem (int x, int y, int[][] fs, int x1) {
-		if (fs[x1][y] == -1)
-		if (y == 0)
-		fs [x1][y] = x;
-		else{
-		fMem (x, y-1, fs, x1);
-		fMem (x+1, y-1, fs, x1+1);
-		fs[x1][y] = fs[x1][y-1] + fs[x1+1][y-1];
-		}
-		}
-		}*/
-	
-	
-/////////////////////////////////////////////////////////////////7
-///////////////////////////////////////////////////////////////77
-	/*public class Practica_4_ER {
-		public static int fTab (int x, int y) {
+	// auxiliar
+	public static int fM(int x, int y) {
+		int[][] fibs = new int[x + 2][y + 2];
+		for (int i = 0; i <= x; i++)
+			for (int z = 0; z <= x; z++)
+				fibs[i][z] = -1;
+		fibM(x, y, 0, fibs);
+		return fibs[0][y];
+	}
+
+///////////////////////////////////////////////////////////////////
+//tabulación //////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+	public static int fT(int x, int y) {
 		int[][] fs = new int[y + 1][y + 1];
 		// Calculamos valor inicial en (0,0)
 		fs[0][0] = x;
-		// Calculamos el resto de valores base de la fila 0
-		for (int i=1; i<=y; i++)
-		fs[i][0] = fs[i-1][0]+1;
-		 Vamos bajando niveles (fila 1, fila 2...) y recorriendo para
-		sumar encada caso su dos valores a los que apunte (hijos). El
-		recorrido de i será hasta y-j porque por cada nivel que bajemos,
-		debemos quedarnos en una posición anterior, para nunca pasar de
-		la diagonal 
-		for (int j=1; j<=y; j++){
-		for (int i=0; i<=y-j; i++)
-		fs[i][j] = fs[i][j-1] + fs[i+1][j-1];
+		for (int i = 1; i <= y; i++)
+			fs[i][0] = fs[i - 1][0] + 1;
+		for (int j = 1; j <= y; j++) {
+			for (int i = 0; i <= y - j; i++)
+				fs[i][j] = fs[i][j - 1] + fs[i + 1][j - 1];
 		}
 		return fs[0][y];
-		}
-		}*/
-	
+	}
+		
 	
 	
 	public static void main(String[] args) {
-		System.out.println(f(5,3));
-		System.out.println(fib(5,3));
-		//System.out.println(f2(5,3));
-		//System.out.println(fi(4));
-		//System.out.println(f2(4));
+		System.out.println(f(3,3));
+		System.out.println(fM(3,3));
+		System.out.println(fT(3,3));
 	}
 
 }
